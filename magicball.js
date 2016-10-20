5639d6f48d2aa8e01900ba1549091ef1c987fce6
Kendra Egert <Kendra Egert>
2016-10-20 11:44:11 -0400

    initial and final commit

---
+++ magicball.js
@@ -0,0 +1,12 @@
+﻿window.onload = function () {
+   
+var answers = [
+  'Yes, but you\'re gonna need a stiff drink', 'No and Not EVER','Seriously?! You want that',
+'Maybe and I mean MAYBE','Yes ummmmm Yes!'];
+
+
+document.getElementById('answerBall').onclick = function () {
+    var answer = answers[Math.floor(Math.random() * answers.length)];
+    document.getElementById('magic').innerHTML = answer;
+};
+};
